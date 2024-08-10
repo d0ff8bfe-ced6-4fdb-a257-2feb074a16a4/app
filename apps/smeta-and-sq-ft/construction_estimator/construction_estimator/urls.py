fc0e3efd15b2848f03d1a0611f7e3e38e528f222
Далер <101361201+DarkSteelD@users.noreply.github.com>
2024-08-10 09:11:17 -0900

    feat: Add endpoint for getting materials

--- apps/smeta-and-sq-ft/construction_estimator/construction_estimator/urls.py
+++ apps/smeta-and-sq-ft/construction_estimator/construction_estimator/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from estimator.views import estimate_view
+from estimator.views import estimate_view, get_materials
 
 urlpatterns = [
     path('', estimate_view, name='estimate'),
+    path('get-materials/', get_materials, name='get_materials'),
 ]
